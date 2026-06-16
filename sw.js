bf3a38cfe93171b425c9cdbb86e26abaa4a626b0
i998979 <brianx2x@gmail.com>
2026-06-16 18:15:36 +0800

    Update

--- sw.js
+++ sw.js
@@ -1,9 +1,17 @@
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-});
+const CACHE_NAME = 'v1';
+
+self.addEventListener('install', (event) => self.skipWaiting());
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+    if (event.request.method !== 'GET') return;
+
+    event.respondWith(fetch(event.request)
+        .then((res) => {
+            if (res.status === 200) {
+                const clone = res.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+            }
+            return res;
+        })
+        .catch(() => caches.match(event.request)));
 });
